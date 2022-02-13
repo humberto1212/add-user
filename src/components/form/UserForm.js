bc5c35f40f7b11bfe89687dac897f7fb9ccdcfc7
Humberto <humberto@Humbertos-MacBook-Air.local>
2022-02-13 15:51:43 +0100

    import useState, add states, add Button, create handles, add flex

--- src/components/form/UserForm.js
+++ src/components/form/UserForm.js
@@ -1,14 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button'
 
 
-export default function UserForm() {
+export default function UserForm(props) {
+
+    const [newName, setNewName] = useState('')
+    const [newAge, setNewAge] = useState('')
+  
+    const handleName = (event) => {
+        
+        return setNewName(event.target.value)
+    }
+
+    const handleAge = (event) => {
+        return setNewAge(event.target.value) 
+    }
+  
+
+    
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log(newName)
+        console.log(newAge)
+    }   
+
+
   return (
     <div>
-        <form>
-
-        </form>
+            <Box
+            component="form"
+            onSubmit={handleSubmit}
+            sx={{
+                '& > :not(style)': { m: 1, width: '25ch' },
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}
+            noValidate
+            autoComplete="off"
+            > 
+                <TextField 
+                id="name" 
+                label="Name" 
+                variant="outlined"
+                onChange={handleName}/>
+                <TextField 
+                id="age" 
+                label="Age" 
+                variant="outlined" 
+                onChange={handleAge}
+                />
+                <Button type="submit" variant="outlined">Create New User</Button>
+            </Box>
     </div>
   )
 }
