fec5910ba925fe050fe4ac9d98478178453e294b
Humberto <humberto@Humbertos-MacBook-Air.local>
2022-02-13 12:48:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React , {useState} from 'react'
+import UserList from './components/user/UserList'
+import UserForm from './components/form/UserForm'
+
 
 function App() {
+  const [usersList, setUsersList] = useState([])
+  
+  const addUser = (newUser) => {
+    setUsersList((prevUser) => {
+      return [newUser, ...prevUser]
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+
+      <UserForm
+        onAddUser={addUser}
+      />
+
+       <UserList
+        onUsersList={usersList}
+       /> 
     </div>
   );
 }
---
+++ src/components/form/UserForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+
+export default function UserForm() {
+  return (
+    <div>
+        <form>
+
+        </form>
+    </div>
+  )
+}
