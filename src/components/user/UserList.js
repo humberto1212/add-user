6b8c5c83f3a3dcbed3af9850bc5e82e572b919db
Humberto <humberto@Humbertos-MacBook-Air.local>
2022-02-15 19:06:52 +0100

    map on users list

--- src/components/user/UserList.js
+++ src/components/user/UserList.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import User from './User'
 
-export default function UserList() {
+export default function UserList(props) {
   return (
     <div>
-      <User />
+      {
+        props.onUsersList.map((user, id) => {
+          return (
+            <div key={id}>
+              <User onName={user.name} onAge={user.age} />
+            </div>
+            
+          )
+        })
+      }
     </div>
   )
 }
