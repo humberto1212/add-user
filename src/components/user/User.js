cfc2a87dcbb77b463b900242507f08757cc293b9
Humberto <humberto@Humbertos-MacBook-Air.local>
2022-02-15 19:06:10 +0100

    call props

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
-export default function User() {
+export default function User(props) {
+
+
   return (
-    <div>User</div>
+    <>
+      <div>
+        Name: {props.onName}
+      </div>
+      <div>
+        Age: {props.onAge}
+      </div>
+    </>
   )
 }
