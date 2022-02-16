618a65aecd6340e7df3d82db537214b2c040ec5f
Humberto <humberto@Humbertos-MacBook-Air.local>
2022-02-16 21:34:10 +0100

    add MUI list

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,16 +1,37 @@
-import React from 'react'
+import * as React from 'react';
+
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import PersonIcon from '@mui/icons-material/Person';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function User(props) {
 
-
   return (
     <>
-      <div>
-        Name: {props.onName}
-      </div>
-      <div>
-        Age: {props.onAge}
-      </div>
+            <List>
+                <ListItem
+                  secondaryAction={
+                    <IconButton edge="end" aria-label="delete">
+                      <DeleteIcon />
+                    </IconButton>
+                  }
+                >
+                  <ListItemAvatar>
+                    <Avatar>
+                    <PersonIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={`Name: ${props.onName}`}
+                    secondary={props.onAge ? `Age: ${props.onAge}` : null}
+                  />
+                </ListItem>
+            </List>
     </>
   )
 }
